2912818aa583908ad06484c22a96ab5e409db5fd
Nourin-020 <akternourin123@gmail.com>
2021-11-03 20:15:31 +0600

    Complete Design

--- js/style.js
+++ js/style.js
@@ -1,3 +1,4 @@
+// Search icon
 const searchBtn = document.querySelector(".search-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
 const searchBox = document.querySelector(".search-box");
@@ -14,3 +15,23 @@ cancelBtn.onclick = () => {
     searchBtn.classList.remove("active");
     cancelBtn.classList.remove("active");
 }
+// Counter Section
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id,start,end,duration){
+        let obj= document.getElementById(id),
+        current=start,
+        range = end-start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration/range)),
+        timer= setInterval(() => {
+            current += increment;
+            obj.textContent = current;
+            if(current == end){
+                clearInterval (timer);
+            }
+        },step);
+    }
+    counter("count1",0,1240,3000);
+    counter("count2",0,2000,3000);
+    counter("count3",0,1000,3000);
+});
